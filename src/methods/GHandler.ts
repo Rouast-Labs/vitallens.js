cec8b9b9c1d2899a7d7b90d75604c8c9c10e1063
Philipp Rouast <philipp@rouast.com>
2026-02-01 21:05:21 +1100

    Debugging

--- src/methods/GHandler.ts
+++ src/methods/GHandler.ts
@@ -40,14 +40,14 @@ export class GHandler extends SimpleMethodHandler {
   /**
    * Postprocess the estimated signal.
    * Applies detrending and standardization.
-   * @param signalType The type of signal ('ppg' or 'resp').
+   * @param signalType The signal type (irrelevant here - always ppg_waveform).
    * @param data The raw estimated signal.
    * @param fps The sampling frequency.
    * @param light Whether to do only light processing.
    * @returns The filtered pulse signal.
    */
   postprocess(
-    signalType: 'ppg' | 'resp',
+    signalType: string,
     data: number[],
     fps: number,
     light: boolean
